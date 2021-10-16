67317aaeac071399083f62bf07e0a219442fd2cb
Petr.Bartunek <petr.bartunek@firma.seznam.cz>
2021-10-16 07:03:01 +0200

    csx_stats

--- Dockerfile.php
+++ Dockerfile.php
@@ -3,4 +3,4 @@ MAINTAINER Petr Bartunek <petr.bartunek@firma.seznam.cz>
 
 RUN docker-php-ext-install pdo_mysql
 
-COPY --chown=www-data csx_stats /csx_stats
+COPY --chown=www-data:www-data csx_stats /csx_stats
