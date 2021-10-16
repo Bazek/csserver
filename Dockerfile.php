fbad9f7d213968c9affddeedf93278f5409d5c63
Petr.Bartunek <petr.bartunek@firma.seznam.cz>
2021-10-16 08:03:07 +0200

    csx_stats

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,6 +1,6 @@
 FROM php:fpm-alpine
 MAINTAINER Petr Bartunek <petr.bartunek@firma.seznam.cz>
 
-RUN docker-php-ext-install pdo_mysql
+RUN apk add gmp-dev && docker-php-ext-install pdo_mysql gmp
 
 COPY --chown=www-data:www-data csx_stats /csx_stats
