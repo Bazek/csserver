9d755418abe2d6467d2e917745b8df52dcb7b9ad
Petr.Bartunek <petr.bartunek@firma.seznam.cz>
2022-01-06 13:07:44 +0100

    __STEAM_API_KEY__

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,6 +1,8 @@
 FROM php:fpm-alpine
 MAINTAINER Petr Bartunek <petr.bartunek@firma.seznam.cz>
+ARG STEAM_API_KEY
 
 RUN apk add gmp-dev && docker-php-ext-install pdo_mysql gmp
 
 COPY --chown=www-data:www-data csx_stats /csx_stats
+RUN sed s/__STEAM_API_KEY__/$STEAM_API_KEY/g -i /csx_stats/private/includes/_config.inc
